e48fa0ec5a68bd517641daa148ad3e31d55c08f4
Michael <mvale005@gmail.com>
2022-10-07 16:51:12 -0500

    grid is working with color chaging squares

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
 
+// this is created a 16 x 16 square grid
 
 for(let i = 0; i<256;i++){
-
 let square = document.createElement("div");
+square.className = "square-color";
 square.style.width = "35.5px";
 square.style.height = "35.5px";
 square.style.borderStyle = "solid";
@@ -12,7 +13,20 @@ square.style.borderWidth = "1px";
 
 
 let container = document.querySelector(".bodyContainer");
+
 container.appendChild(square);
 }
 
+let squares = document.querySelectorAll(".square-color");
+
+
+for (const square of squares) {
+    square.addEventListener('mouseover', changeColor)
+
+    function changeColor(e){
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        square.style.backgroundColor = "#" + randomColor;
+       
+    }
+  }
 
