14d5f99c33a9b761c894a32a3c93b535420dbd3f
Michael <mvale005@gmail.com>
2022-10-07 15:38:40 -0500

    created 16x16 grid of divs

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+for(let i = 0; i<256;i++){
+
+let square = document.createElement("div");
+square.style.width = "35.5px";
+square.style.height = "35.5px";
+square.style.borderStyle = "solid";
+square.style.borderColor = "black";
+square.style.borderWidth = "1px";
+
+
+let container = document.querySelector(".bodyContainer");
+container.appendChild(square);
+}
+
+
